2f95128f8e95dfe12494fe5e94cbd030896f8925
Marcus Shannon <marcusshannon@Marcuss-MacBook-Pro.local>
2017-01-05 18:38:36 -0400

    v1

---
+++ app.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var moment = require('moment')
+require('dotenv').config();
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+app.get('/bundle.js', function(req, res) {
+  res.sendFile(path.join(__dirname, 'bundle.js'));
+});
+app.get('/style.css', function(req, res) {
+  res.sendFile(path.join(__dirname, 'style.css'));
+});
+
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+  timezone: '-04:00'
+});
+
+var Day = sequelize.define('Day', {
+  date: {type: Sequelize.DATEONLY, defaultValue: Sequelize.NOW},
+  A: {type: Sequelize.INTEGER, defaultValue: 0},
+  B: {type: Sequelize.INTEGER, defaultValue: 0},
+  C: {type: Sequelize.INTEGER, defaultValue: 0},
+  D: {type: Sequelize.INTEGER, defaultValue: 0}
+});
+
+sequelize.sync();
+
+function incrementDB(req, res, option) {
+  Day.findOrCreate({
+    where: {
+      date: new Date()
+    }
+  })
+  .spread(function(instance, created) {
+    instance.increment(option);
+    res.set('Access-Control-Allow-Origin', null);
+    res.sendStatus(200);
+  });
+}
+
+app.post('/a', function(req, res) {
+  console.log(req);
+  incrementDB(req, res, 'A');
+});
+
+app.post('/b', function(req, res) {
+  incrementDB(req, res, 'B');
+});
+
+app.post('/c', function(req, res) {
+  incrementDB(req, res, 'C');
+});
+
+app.post('/d', function(req, res) {
+  incrementDB(req, res, 'D');
+});
+
+function calculateValue(obj) {
+  return (obj.A * 3 + obj.B * 2 + obj.C) / (obj.A + obj.B + obj.C + obj.D) / 3;
+}
+
+app.get('/data', function(req, res) {
+  Day.findAll({order: [['date', 'ASC']]}).then(function(data) {
+    res.send(data.map(obj => { return {date: new moment(obj.date), A: obj.A, B: obj.B, C: obj.C, D: obj.D, value: calculateValue(obj)}}));
+  });
+});
+
+app.listen(process.env.PORT || 3000);
---
+++ index.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+var rp = require('request-promise');
+var moment = require('moment');
+
+class Logo extends React.Component {
+  render() {
+    return (
+      <h1 className="display-3">{this.props.name}</h1>
+    );
+  }
+}
+
+class Graph extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    rp(process.env.URL + 'data').then(function(res) {
+      this.setState({
+        data: JSON.parse(res).map(x => {x.date = new Date(x.date); return x}).sort((a, b) => a.date > b.date)
+      });
+      var labels = this.state.data.map(x => new moment(x.date).format("MMM D"));
+      var series = [this.state.data.map(x => x.value)];
+      var data = {labels: labels, series: series};
+      var options = {
+        fullWidth: true,
+        height: 500,
+        low: 0,
+        high: 1,
+        showArea: true,
+        showPoint: false,
+        chartPadding: {
+          right: 50
+        },
+        axisX: {
+          showGrid: true
+        },
+        axisY: {
+          showGrid: false,
+          showLabel: false
+        }
+      };
+      var chart = new Chartist.Line('.ct-chart', data, options);
+    }.bind(this));
+  }
+  render() {
+    return (<div className="ct-chart"></div>);
+  }
+}
+
+
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    var post = new XMLHttpRequest();
+    post.open("POST", process.env.URL + this.props.value);
+    post.send();
+  }
+
+  render() {
+    return (
+      <div className="button col-md-6" onClick={this.handleClick}>{this.props.emoji}</div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <div>
+    <Logo name="Raptors"/>
+    <Graph/>
+    <div className="row">
+      <Button value="a" emoji="😁"/>
+      <Button value="b" emoji="🙂"/>
+    </div>
+    <div className="row">
+      <Button value="c" emoji="🙁"/>
+      <Button value="d" emoji="😠"/>
+    </div>
+  </div>,
+  document.getElementById('root')
+);
