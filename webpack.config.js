f5198d194e400e0b03534e94f2b56cf086239d5c
Marcus Shannon <marcusshannon@Marcuss-MacBook-Pro.local>
2017-01-11 18:50:08 -0500

    babel

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,6 @@ module.exports = {
   ],
   module: {
     loaders: [
-      { test: /\.js$/, loader: "babel" },
       { test: /\.jsx$/, loader: "babel" },
       { test: /\.json$/, loader: "json" }
     ]
